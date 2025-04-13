589dc86f25be43f5d5cbd5f038da60f16de44f0e
Peter Gituma <pgituma@myseneca.ca>
2025-04-13 23:19:38 +0100

    Removed trim case in test-data files.

--- login-playwright/tests/loginNegative.test.js
+++ login-playwright/tests/loginNegative.test.js
@@ -7,7 +7,7 @@ test.describe('Login Tests', () => {
     test(`Login with valid credentials: ${data.username} - ${index + 1}`, async ({ page }) => {
       const loginPage = new LoginPage(page);
       await page.goto("http://127.0.0.1:5500/login-playwright/static/Login.html");
-      await loginPage.login(data.username.trim(), data.password.trim());  // Trim in case of extra spaces
+      // await loginPage.login(data.username.trim(), data.password.trim());  // Trim in case of extra spaces
 
       expect(await loginPage.isLoginSuccessful());
     });
